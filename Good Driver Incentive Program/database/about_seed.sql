9f7d96b916a8629e22944f0d3b5dd256328694af
hadvani3 <sadvani0703@gmail.com>
2026-02-13 10:23:10 -0500

    updated about page for sprint 3

--- Good Driver Incentive Program/database/about_seed.sql
+++ Good Driver Incentive Program/database/about_seed.sql
@@ -10,17 +10,23 @@ product_name,
 product_desc,
 is_curr
 )
+
 SELECT 
 	'Team 3',
-    'Sprint 2',
+    'Sprint 3',
     CURDATE(),
     'Good Driver Incentive Program',
     'A full-stack web application designed to reward drivers for good driving habits.',
     TRUE
-    
+
 WHERE NOT EXISTS (
 	SELECT 1
     FROM about_info
-    WHERE team_number = 'Team 3' AND version_number = 'Sprint 2'
+    WHERE team_number = 'Team 3' AND version_number = 'Sprint 3'
 );
+
+-- ensure that only sprint 3 row is inserted
+UPDATE about_info
+SET is_curr = (version_number = 'Sprint 3')
+WHERE team_number = 'Team 3';
     
