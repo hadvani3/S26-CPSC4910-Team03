5de34b79c8552daf7f9a4f1a932a0e5edbd23a18
Pizzasauce3274 <currenp@g.clemson.edu>
2026-03-02 12:25:20 -0500

    Home Page Modifications
    Home page has basic summary and details provided.

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -8,6 +8,24 @@ export default function Home() {
                 <h1>Home</h1>
                 <p className="item"> Welcome to the home page!</p>
             </div>
+            <div className="wrap">
+                <div className="home-test">
+                    <h1>Basic Information</h1>
+                    <p className = "item">
+                        Here the driver incentive program rewards diligent behavior
+                        and outstanding work in the form of points. Points can be
+                        utilized to buy certain rewards that are listed within the
+                        product catalog.
+                    </p>
+                </div>
+                <div className="home-test">
+                    <h1>Catalog</h1>
+                    <p className = "item">
+                        Where you can find various products that are available
+                        for you to spend your points on.
+                    </p>
+                </div>
+            </div>
         </>
     );
 }
