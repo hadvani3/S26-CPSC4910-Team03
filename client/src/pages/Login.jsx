3b126b388a343ab247bce9ba049cc1340d55c37a
ben-schwarz <ben@k-schwarz.com>
2026-03-03 11:40:03 -0500

    Update login

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -11,7 +11,7 @@ export default function Login() {
     e.preventDefault();
 
     try {
-      const res = await fetch("https://team03.cpsc4911.com/login", {
+      const res = await fetch("https://team03.cpsc4911.com/", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
