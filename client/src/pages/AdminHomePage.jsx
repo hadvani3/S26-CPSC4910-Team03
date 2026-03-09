532da30f92529551700b2d36a5e546330ff024e8
Pizzasauce3274 <currenp@g.clemson.edu>
2026-03-09 15:14:41 -0400

    Admin User Home Page
    The basic implementation of the admin login page

---
+++ client/src/pages/AdminHomePage.jsx
@@ -0,0 +1,57 @@
+export default function AdminHomePage() {
+    return (
+        <>
+        <div class = "home-test">
+            <ul>
+                <li>Welcome back!: </li>
+            </ul>
+        </div>
+
+        <div class = "wrap">
+            <div class = "home-test">
+                <h1>Drivers In The System</h1>
+                <ul>
+                    <li>Driver_1</li>
+                    <li>Driver_2</li>
+                    <li>Driver_3</li>
+                    <li>Driver_4</li>
+                    <li>Driver_5</li>
+                    <li>Driver_6</li>
+                </ul>
+            </div>
+
+            <div class = "home-test">
+                <h1>Sponsors In The System</h1>
+                <ul>
+                    <li>Sponsor_a</li>
+                    <li>Sponsor_b</li>
+                    <li>Sponsor_c</li>
+                    <li>Sponsor_d</li>
+                    <li>Sponsor_e</li>
+                    <li>Sponsor_f</li>
+                </ul>
+            </div>
+        </div>
+        <div class = "wrap">
+            <div class = "home-test">
+                <h1>Create Driver Users</h1>
+                <button type = "button">Click Here</button>
+            </div>
+            <div class = "home-test">
+                <h1>Delete Driver Users</h1>
+                <button type = "button">Click Here</button>
+            </div>
+        </div>
+        <div class = "wrap">
+            <div class = "home-test">
+                <h1>Create Sponsor Users</h1>
+                <button type = "button">Click Here</button>
+            </div>
+            <div class = "home-test">
+                <h1>Delete Sponsor Users</h1>
+                <button type = "button">Click Here</button>
+            </div>
+        </div>
+        </>
+    );
+}
