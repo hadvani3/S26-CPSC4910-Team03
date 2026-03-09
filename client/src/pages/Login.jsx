a6deb46f6ca2a9aa93ace7b1ade7050a0f3c2c6d
ben-schwarz <ben@k-schwarz.com>
2026-03-09 18:12:33 -0400

    Updated login fetch

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -14,7 +14,7 @@ export default function Login() {
     e.preventDefault();
 
     try {
-      const res = await fetch("100.51.75.41/login", {
+      const res = await fetch("https://100.51.75.41:3000/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
