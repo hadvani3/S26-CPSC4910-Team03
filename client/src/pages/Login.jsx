66af78a3361b7fb737ae113f8f359d2d2499416a
ben-schwarz <ben@k-schwarz.com>
2026-03-09 18:05:45 -0400

    Change login fetch url to ec2

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -14,7 +14,7 @@ export default function Login() {
     e.preventDefault();
 
     try {
-      const res = await fetch("http://localhost:3000/login", {
+      const res = await fetch("100.51.75.41/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
