94500b73e491ef42b480e5aecc08238dce22d794
hadvani3 <sadvani0703@gmail.com>
2026-03-24 09:31:30 -0400

    sprint 7 - about info

--- database/about_seed.sql
+++ database/about_seed.sql
@@ -13,20 +13,20 @@ is_curr
 
 SELECT 
 	'Team 3',
-    'Sprint 6',
+    'Sprint 7',
     CURDATE(),
     'Good Driver Incentive Program',
-    'Sprint 6 Changes - Our web application now includes fully functional login for all user types. The product catalog list is functional as well.',
+    'Sprint 7 Changes - Implemented core features for drivers, sponsors and admin users. Product catalog is also functional. ',
     TRUE
 
 WHERE NOT EXISTS (
 	SELECT 1
     FROM about_info
-    WHERE team_number = 'Team 3' AND version_number = 'Sprint 6'
+    WHERE team_number = 'Team 3' AND version_number = 'Sprint 7'
 );
 
--- ensure that only Sprint 6 data is inserted as current version
+-- ensure that only Sprint 7 data is inserted as current version
 UPDATE about_info
-SET is_curr = (version_number = 'Sprint 6')
+SET is_curr = (version_number = 'Sprint 7')
 WHERE team_number = 'Team 3';
     
