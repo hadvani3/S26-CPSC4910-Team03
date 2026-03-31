28eb5c60d382183def9439719dd9aefa905e425f
ben-schwarz <ben@k-schwarz.com>
2026-03-31 14:11:12 -0400

    Changed createaccount fetch to ec2 rather than localhost

--- client/src/pages/CreateAccount.jsx
+++ client/src/pages/CreateAccount.jsx
@@ -24,7 +24,7 @@ export default function CreateAccount() {
     setLoading(true);
 
     try {
-      const res = await fetch("http://localhost:3000/create_account", {
+      const res = await fetch("https://team03.cpsc4911.com/create_account", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
